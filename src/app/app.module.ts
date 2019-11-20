85f8a57d044d9103f21392490e69c7d5c56c5fb2
Shashank Shah <shshah@extremenetworks.com>
2019-11-20 16:35:50 +0530

    chip search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FilterPipe} from './filter.pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ChipSearchComponent } from './chip-search/chip-search.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChipSearchComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chip-search/chip-search.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chip-search',
+  templateUrl: './chip-search.component.html',
+  styleUrls: ['./chip-search.component.css']
+})
+export class ChipSearchComponent implements OnInit {
+
+  myItem:string ="";
+
+  selectedList:any = [];
+
+  list:any= []
+
+  lastSelected:boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+    this.list = ["John", "Allen", "Gracy", "Nicole"]
+  }
+  deleteItem(event:any,a,b){
+    if (!this.lastSelected)
+      this.lastSelected =true;
+    else
+      this.removeItem(b,a);
+  }
+  appendToList(itm,i){
+    if (this.selectedList.indexOf(itm)<0){
+      this.selectedList.push(itm);
+      this.list.splice(i,1);
+    }
+  }
+
+  removeItem(i,itm){
+    this.selectedList.splice(i,1);
+    this.list.push(itm);
+    this.lastSelected =false;
+  }
+
+}
